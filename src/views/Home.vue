4ed920bc000612e1039cebc0e7a5363b908c0b94
Bill Cathey <billcathey@hotmail.com>
2021-08-15 22:56:40 -0400

    Add method to fetch data, put data into state

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,41 @@
 <template>
-  <div class="home">
-    Hello World!
-  </div>
+  <main v-if="!loading">
+    Show Data
+  </main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="mt-10 mb-6 text-3xl text-gray-500">Fetching Data</div>
+    <img :src="loadingImage" class="w-24 m-auto" />
+  </main>
 </template>
 
 <script>
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    console.log(data);
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
 };
 </script>
